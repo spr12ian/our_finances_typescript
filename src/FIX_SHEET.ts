5760089a3db073eddcbf2cbb37b8de5b539292d3
spr12ian <173560+spr12ian@users.noreply.github.com>
2025-09-08 23:06:35 +0100

    work on FIX_SHEET

--- src/FIX_SHEET.ts
+++ src/FIX_SHEET.ts
@@ -1,14 +1,12 @@
-import { getFinancesSpreadsheet } from "./getFinancesSpreadsheet";
-import { OurFinances } from "./OurFinances";
+import { getExtendedSheet } from "./getExtendedSheet";
 import type { ParamsOf } from "./queueTypes";
 import { FastLog } from "./support/FastLog";
 
 export function FIX_SHEET(parameters: ParamsOf<"FIX_SHEET">): void {
   FastLog.log("Started FIX_SHEET", parameters);
-  // const { sheetName } = parameters;
+  const { sheetName } = parameters;
 
-  const spreadsheet = getFinancesSpreadsheet();
-  const ourFinances = new OurFinances(spreadsheet);
-  ourFinances.fixSheet();
+  const sheet = getExtendedSheet(sheetName);
+  sheet.fixSheet();
   FastLog.log("Finished FIX_SHEET", parameters);
 }

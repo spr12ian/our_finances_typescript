6220354ce6c681042fce7e6e6bf8b4842d59a544
spr12ian <173560+spr12ian@users.noreply.github.com>
2025-11-12 22:42:07 +0000

    HMRC B

--- src/features/sheets/classes/index.ts
+++ src/features/sheets/classes/index.ts
@@ -4,5 +4,6 @@ export { Assets } from "./Assets";
 export { BankAccounts } from "./BankAccounts";
 export { Dependencies } from "./Dependencies";
 export { DescriptionReplacements } from "./DescriptionReplacements";
+export { HMRC_B } from "./HMRC_B";
 export { HMRC_S } from "./HMRC_S";
 export { OurMoney } from "./OurMoney";
